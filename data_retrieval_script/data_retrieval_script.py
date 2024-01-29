226aef99afbd31a7128778b879a9a76b2fa009a1
olin_tp_ar <universalexperimental@protonmail.com>
2024-01-29 12:28:55 +0100

    Addition of a single user to a collection using the python script

--- data_retrieval_script/data_retrieval_script.py
+++ data_retrieval_script/data_retrieval_script.py
@@ -1,6 +1,7 @@
 import requests
 from pymongo import MongoClient
 
+print('SCRIPT')
 mongo_host='mongo'
 mongo_port=27017
 mongo_db='myDB'
@@ -11,5 +12,10 @@ api_url='https://jsonplaceholder.typicode.com/users'
 client = MongoClient(mongo_host, mongo_port)
 db = client[mongo_db]
 collection = db[mongo_collection]
+
+user = { "name": "Alex", "lastname": "Jackson"}
+users = db.users
+user_id = users.insert_one(user).inserted_id
+
 
 print('DONE SCRIPT')
