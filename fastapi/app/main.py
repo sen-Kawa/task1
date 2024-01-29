7fa34c62ce858e263cd555672de4de59f8c39d9a
olin_tp_ar <universalexperimental@protonmail.com>
2024-01-29 16:52:44 +0100

    FastAPI: Defining a function to be called by all routes in order to retrieve data from mongoDB

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -6,20 +6,24 @@ app = FastAPI()
 mongo_host = 'mongo'
 mongo_port = 27017
 mongo_db = 'myDB'
-mongo_collection = 'Todos'
 
 client = MongoClient(mongo_host, mongo_port)
 db = client[mongo_db]
+
+def read_data(collection_name):
+    collection = db[collection_name]
+    data = []
+    for item_data in collection.find():
+        del item_data['_id']
+        data.append(item_data)
+    return data
 
 @app.get("/")
 def index():
     return {"hello": "world"}
 
 @app.get("/todos")
-def read_data():
-    collection = db[mongo_collection]
-    data = []
-    for item_data in collection.find():
-        del item_data['_id']
-        data.append(item_data)
-    return data
+def read_todos():
+    return read_data('Todos')
+
+
